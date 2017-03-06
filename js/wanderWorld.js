64e623fef78a0de1529a7749bb85ecc3a60a5741
juliafalvey3 <julia.falvey@gatech.edu>
2017-03-06 17:28:00 -0500

    working on Europe map

--- js/wanderWorld.js
+++ js/wanderWorld.js
@@ -1,22 +1,40 @@
 function init(){
 
-  var center, countries, height, path, projection, scale, svg, width;
-  width = 300;
-  height = 400;
-  center = [5, 70];
-  scale = 600;
-  projection = d3.geo.mercator().scale(scale).translate([width / 2, 0]).center(center);
-  path = d3.geo.path().projection(projection);
-  svg = d3.select("#map").append("svg").attr("height", height).attr("width", width);
-  countries = svg.append("g");
-  d3.json("eu.topojson", function(data) {
-    countries.selectAll('.country')
-    .data(topojson.feature(data, data.objects.europe).features)
-    .enter()
-    .append('path')
-    .attr('class', 'country')
-    .attr('d', path);
-    return;
-  });
+
+	//Width and height
+	var width = 800;
+	var height = 600;
+
+	var projection = d3.geo.satellite()
+		.translate([width/2,0])
+		.distance(1.1)
+		.scale(3250)
+		.center([30, 50])
+		.rotate([3, -51.3849401, -45])
+		.tilt(20)
+		.clipAngle(Math.acos(1 / 1.1) * 180 / Math.PI - 1e-6);
+		
+	var path = d3.geo.path()
+		.projection(projection);
+		
+	var graticule = d3.geo.graticule()
+	    .step([5, 5]);
+		
+	var svg = d3.select("body").append("svg")
+		.attr("width", width)
+		.attr("height", height);
+		
+	svg.append("path")
+		.datum(graticule)
+		.attr("class", "graticule")
+		.attr("d", path);
+		
+	var land = svg.append("g");
+	d3.json('europe.json', function(err, data) {
+		land.append("path")
+			.datum(topojson.object(data, data.objects.europe))
+			.attr("class", "land")
+			.attr("d", path);
+	});
 	
 }
