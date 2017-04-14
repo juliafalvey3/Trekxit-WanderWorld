9cc911dfb6aaecd7735a1e82cbce31c63c0ae507
Gabriella Runnels <grunnels@tulane.edu>
2017-04-14 17:06:22 -0400

    Gabriella changes

--- wanderWorld.js
+++ wanderWorld.js
@@ -93,7 +93,6 @@ function makeOthers(container){
 		.attr('width', 390)
 		.attr('height', 800)
 
-	// claraWork(others)
 	juliaWork(others)
 
 }
@@ -108,10 +107,9 @@ function juliaWork(others){
 		.attr("x", 20)
 		.attr("y", 30)
 
-	var startCities = ["Select a Starting City", "Atlanta", "Boston", "New York", "Orlando"];
-	// var startCities = ["Atlanta", "Boston", "New York", "Orlando"];
-	// 	startCities.sort(function(x, y){ return d3.ascending(x, y);})
-	// 	startCities.unshift(“Select a Starting City”)
+    var startCities = ["Atlanta", "Boston", "New York", "Orlando"];
+    startCities.sort(function(x, y){ return d3.ascending(x, y);})
+    startCities.unshift("Select a Starting City")
 
 	var select = d3.select('body')
   		.append('select')
@@ -128,103 +126,9 @@ function juliaWork(others){
 		console.log(selectValue)
 	}
 
-	var destCities = ['Barcelona', 'Madrid', 'Dublin', 'London', 'Edinburgh', 'Berlin', 'Paris', 'Marseilles'];
-	//Sort List
-	destCities.sort(function(x, y){ return d3.ascending(x, y);})
-	//Get Length of List
-	var destLen = destCities.length;
-	//Subset half of list
-	var dest1 = destCities.slice(0,Math.ceil(destLen/2))
-	//Get other half
-	var dest2 = destCities.slice(Math.ceil(destLen/2), destLen)
-
-	var buttons = others.append('g')
-		.attr('id', 'checkboxes');
-
 	var defaultColor= "#ebebeb";
     var hoverColor= "darkgrey";
     var pressedColor= "grey";
-
-	var cityButtons = buttons.selectAll("g")
-		.data(dest1)
-		.enter().append('g')
-		.attr('id', function(d){return d})
-		.attr("stroke", '#252525')
-		.on("click",function(d,i) {
-				if (d3.select(this).select("rect").attr("fill") != pressedColor) {
-                    d3.select(this).select("rect").attr("fill", pressedColor);
-                    console.log(d)
-             	}
-				else {
-					d3.select(this).select("rect").attr("fill", defaultColor)
-					console.log("removing "+d)
-			}})
-        .on("mouseover", function() {
-                if (d3.select(this).select("rect").attr("fill") != pressedColor) {
-                    d3.select(this).select("rect").attr("fill", hoverColor);
-             	}})
-        .on("mouseout", function() {
-                if (d3.select(this).select("rect").attr("fill") != pressedColor) {
-                    d3.select(this).select("rect").attr("fill",defaultColor);
-                }})
-	
-	 cityButtons.append("rect")
-        .attr("width", 12)
-        .attr("height", 12)
-        .attr("x", 30)
-        .attr("y", function(d,i){return (i*30 + 125)})
-        .attr("rx", 3) //rx and ry give the buttons rounded corners
-        .attr("ry", 3)
-        .attr("fill",defaultColor)
-
-	cityButtons.append('text')
-		.attr("class", "text")
-		.attr('stroke', 'none')
-		.attr('y', function(d,i){return (i*30 + 136)})
-		.attr('x', 50)
-		.text(function(d){return d});
-
-	var buttons2 = others.append('g')
-		.attr('id', 'checkboxes');
-
-	var cityButtons2 = buttons2.selectAll("g")
-		.data(dest2)
-		.enter().append('g')
-		.attr('id', function(d){return d})
-		.attr("stroke", '#252525')
-		.on("click",function(d,i) {
-				if (d3.select(this).select("rect").attr("fill") != pressedColor) {
-                    d3.select(this).select("rect").attr("fill", pressedColor);
-                    console.log(d)
-             	}
-				else {
-					d3.select(this).select("rect").attr("fill", defaultColor)
-					console.log("removing "+d)
-			}})
-        .on("mouseover", function() {
-                if (d3.select(this).select("rect").attr("fill") != pressedColor) {
-                    d3.select(this).select("rect").attr("fill", hoverColor);
-             	}})
-        .on("mouseout", function() {
-                if (d3.select(this).select("rect").attr("fill") != pressedColor) {
-                    d3.select(this).select("rect").attr("fill",defaultColor);
-                }})
-
-    cityButtons2.append("rect")
-        .attr("width", 12)
-        .attr("height", 12)
-        .attr("x", 150)
-        .attr("y", function(d,i){return (i*30 + 125)})
-        .attr("rx", 3) //rx and ry give the buttons rounded corners
-        .attr("ry", 3)
-        .attr("fill",defaultColor)
-
-	cityButtons2.append('text')
-		.attr("class", "text")
-		.attr('stroke', 'none')
-		.attr('y', function(d,i){return (i*30 + 136)})
-		.attr('x', 170)
-		.text(function(d){return d});
 
 	destinationText = others.append("text");
 
@@ -234,7 +138,8 @@ function juliaWork(others){
 		.attr("y", 100)
 
 	helpDestinationText = others.append("text");
-	helpDestinationText.text("Select up to 5 cities.")
+
+    helpDestinationText.text("Select up to 5 cities.")
 		.attr("class", "helper")
 		.attr("x", 20)
 		.attr("y", 115)
@@ -244,7 +149,7 @@ function juliaWork(others){
 		.attr("class", "question")
 		.attr("x", 20)
 		.attr("y", 260);
-	
+
 	helpdateText = others.append("text");
 	helpdateText.text("Not the spefic days you want to travel but a flexible time frame!")
 		.attr("class", "helper")
@@ -289,7 +194,7 @@ function juliaWork(others){
 		.attr("class", "question")
 		.attr("x", 20)
 		.attr("y", 640);
-	
+
 
 	buttonText = ["Find My Optimal Trips!"]
 	var optimize = others.append('g')
@@ -319,7 +224,7 @@ function juliaWork(others){
         .on("mouseout", function() {
                 if (d3.select(this).select("rect").attr("fill") != pressedColor) {
                     d3.select(this).select("rect").attr("fill",defaultColor)}});
-	
+
 	optimize.append("rect")
 			.attr("width", 275)
 			.attr("height", 30)
@@ -328,7 +233,7 @@ function juliaWork(others){
 			.attr("rx", 3) //rx and ry give the buttons rounded corners
 			.attr("ry", 3)
 			.attr("fill", defaultColor);
-		
+
 	optimize.append('text')
 			.attr('class', 'text')
 			.attr("stroke", "none")
@@ -338,11 +243,10 @@ function juliaWork(others){
 
 }
 
-
 var resultsData = [{'Trip1': '$800'}]
 
 function showboxes(others){
-	
+
 	others.append('rect')
 		.attr('id', 'resultsBox')
 		.attr('fill', 'white')
